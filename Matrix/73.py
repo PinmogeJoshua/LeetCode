ce1df400889a6b3dcaec811d7d5b702fd3765e72
PinmogeJoshua <stellewang0417@qq.com>
2025-03-20 09:18:18 +0800

    Modify Matrix/73.py

--- Matrix/73.py
+++ Matrix/73.py
@@ -11,19 +11,9 @@ class Solutions:
         """
         m = len(matrix)
         n = len(matrix[0])
-        row_zero = col_zero = False
-
-        # 检查第一列是否需要置零 
-        for j in range(n):
-            if matrix[0][j] == 0:
-                row_zero = True
-                break
-
-        # 检查第一列是否需要置零
-        for i in range(m):
-            if matrix[i][0] == 0:
-                col_zero = True
-                break
+        
+        row_zero = any(matrix[0][j] == 0 for j in range(n))  # 检查第一行是否需要置零
+        col_zero = any(matrix[i][0] == 0 for i in range(m))  # 检查第一列是否需要置零
         
         # 先遍历，查找所有为0的位置
         for i in range(1, m):
@@ -37,14 +27,14 @@ class Solutions:
             for j in range(1, n):
                 if matrix[i][0] ==0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-                    
+        
         # 如果第一行需要置零
         if row_zero:
             for j in range(n):
                 matrix[0][j] = 0
-        
+
         # 如果第一列需要置零
         if col_zero:
             for i in range(m):
-                matrix[i][0] = 0
+                matrix[i][0] = 0            
         
